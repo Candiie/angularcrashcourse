7377296648157ec815f6641f9e57142de35a45e3
Candiie <candiiebay@gmail.com>
2017-02-23 00:45:57 +0800

    did npm install & npm start & tried out some variables creation & how it is used in html - to get node modules folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  selector: 'my-app',   //this is what u write in html <my-app>
+  template: `
+  <h1>Hello {{name}}</h1>
+  <p> Email : {{email}} </p>
+  <p> Address: {{address.city}}, {{address.street}} </p>
+  `, //this is a back tag not a quote so can use multiple line
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+    name = 'Angular';
+    email = 'candiie@gmail.com';
+
+    address = {
+        street : 'toa payoh',
+        city : 'singapore'
+    }
+}
