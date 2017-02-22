3561a45a2fe983d6f2ff365755fbcf03a3dc3d6a
Candiie <candiiebay@gmail.com>
2017-02-23 02:10:05 +0800

    extracting html template from .ts file to a separate html file and how to link it via templateUrl and moduleId (in order to use relative paths)

--- src/app/components/user.component.ts
+++ src/app/components/user.component.ts
@@ -3,67 +3,9 @@ import { Component } from '@angular/core';
 import { PostsService } from '../services/post.service';
 
 @Component({
+    moduleId: module.id, //inorder to use relative paths
     selector: 'user',   //this is what u write in html <my-app>
-    template: `
-    <h1>Hello {{name}}</h1>
-    <p> Email : {{email}} </p>
-    <p> Address: {{address.city}}, {{address.street}} </p>
-
-    <!-- when ever you wanna use an event, just use the parenthesis and the functionname -->
-    <!-- in this case we are trying click - (click) -->
-
-    <button (click)="toggleHobbies()"> click me to {{showHobbies ? "Hide Hobbies" : "Show Hobbies" }}</button>
-
-    <!-- you can also do a else in the {{}} << what issit call? anw ^^^^^ refering to the above, if showHobbies is true show "Hide Hobbies" else show "Show Hobbies"-->
-
-    <div *ngIf="showHobbies">
-        <h3> Hobbies </h3>
-
-        <!-- looping through the array -->
-        <ul>
-            <li *ngFor="let hobby of hobbies; let i = index"> <!-- using of ngFor-->
-                {{hobby}} <button (click)="deleteHobby(i)"> X </button>
-            </li>
-        </ul>
-    </div>
-
-    <!-- Inputs and data binding (with ngModel) -->
-
-    <hr/>
-
-    <h2> Edit User Details </h2> <br/>
-
-    <form>
-        <label> Name: </label> <br/>
-        <input type="text" name="name" [(ngModel)]="name" /> <!-- no have ; semi-colon after code in html-->
-        <br/>
-        <br/>
-        <label> City: </label> <br/>
-        <input type="text" name="address.city" [(ngModel)]="address.city" /> <!-- no have ; semi-colon after code in html-->
-
-    </form>
-
-    <br/>
-
-    <!-- Inputs and data binding (without ngModel) -->
-
-    <form (submit)="addHobby(hobby.value)">
-        <label> Add Hobby: </label> <br/>
-        <input type="text" #hobby /> <br/>
-    </form>
-
-
-    <hr/>
-
-    <h3> Posts </h3>
-
-    <div *ngFor="let post of posts">
-        <h3> {{post.title}} </h3>
-
-        <p> {{post.body}} </p>
-    </div>
-
-    `, //this is a back tag not a quote so can use multiple line
+    templateUrl: 'user.component.html', //this is a back tag not a quote so can use multiple line
 
     //add a provider
     providers: [PostsService]
